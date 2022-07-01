de7d35d07ce9e90b5f116cfdd5d92c0f6100a6e5
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-07-01 18:35:05 +0600

    update task by id api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;;
 
@@ -14,16 +14,46 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        
+        const taskCollection = client.db('daily-task').collection('task');
+
+        // get task
+        app.get('/task', async (req, res) => {
+            const query = {};
+            const result = await taskCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        // post task
+        app.post('/task', async (req, res) => {
+            const task = req.body;
+            const result = await taskCollection.insertOne(task);
+            res.send(result);
+        })
+
+        // update task by id
+        app.put('/task/:id', async (req, res) => {
+            const id = req.params.id;
+            const task = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    completed: task.completed
+                },
+            };
+            const result = await taskCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+            console.log(result);
+        })
     }
-    finally{}
+    finally { }
 }
 run().catch(console.dir)
 
 app.get('/', (req, res) => {
     res.send('My Daily Task are running!!!')
-  })
-  
-  app.listen(port, () => {
+})
+
+app.listen(port, () => {
     console.log(`My daily task listening on port ${port}`)
-  })
+})
